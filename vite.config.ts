2dc2be2eeac106187c3442b22e7506f0c198d701
Osama Bsher <65835428+OsamaBsher@users.noreply.github.com>
2025-05-15 12:57:32 +0100

    add base for assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/libyan-social-news/',
   server: {
     host: "::",
     port: 8080,
