4cf6f69ac39c1fa5f3c410ba0046b5a5ee931d1f
Osama Bsher <65835428+OsamaBsher@users.noreply.github.com>
2025-05-15 22:32:02 +0100

    Update base

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/libyan-social-news">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/article/:id" element={<ArticleDetail />} />
